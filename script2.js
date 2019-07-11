a2ac03b2013a53247594c07148b34425d9fd28ac
thakuroda <engkuroda>
2019-07-11 16:47:45 -0300

    criar comandos com console.log - atividade em sala

--- script2.js
+++ script2.js
@@ -20,7 +20,31 @@ while (contador < 12){
 console.log("EXEMPLO COM FOR")
 
 for (let cont = 0; cont < signos.length; cont++) {
+    console.log(signos[cont]);
+    
+}
 
+
+// While é melhor para casos em que não se tenha o número exato de vezes ou tamanho (listas, etc){
+// }
+
+
+let idade = Number(prompt("Digite a sua idade"));
+while (isNaN(Number)){
+    alert("Digite um número");
+    idade = Number (prompt("digite a sua idade"));
+
+}
+
+// ---------For é melhor para casos que se tenha o número exato de vezes ou tamanho (listas, etc){ --------
+// }
+
+for (let i in signos){
+    console.log(signos [i]);
+}
+
+for (let signo of signos){
+    console.log(signo);
 
 
 }
