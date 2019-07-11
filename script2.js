56638a6680c4fa48865aaccfcb815321a3617204
thakuroda <engkuroda>
2019-07-11 16:27:04 -0300

    criar comandos com while e for

--- script2.js
+++ script2.js
@@ -1,1 +1,26 @@
-~kjkçlçl
+// While(condição){
+//    codigo a ser repetido enquanto a condicao for verdadeira
+// }
+
+let signos = ['áries', 'touro', 'gêmeos', 'câncer', 'leão', 'virgem', 'libra', 'escorpião', 'sargitário', 'capricórnio', 'aquário', 'peixes']
+let contador = 0;
+
+//-------------------------Exemplo com While-------------------------------------------------------------------------
+console.log("EXEMPLO COM WHILE")
+
+while (contador < 12){
+    console.log(signos [contador]);
+    // contador = contador +1;
+    // contador += contador;
+    contador++;    
+}
+//-------------------------Exemplo com For---------------------------------------------------------------------------
+// for (inicio; fim; passo){
+// }
+console.log("EXEMPLO COM FOR")
+
+for (let cont = 0; cont < signos.length; cont++) {
+
+
+
+}
