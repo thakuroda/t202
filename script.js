4f8b5a1f3a88af2289d26ef2a2860a1e4421664c
thakuroda <engkuroda>
2019-07-11 15:32:13 -0300

    Criar comandos usando JavaScript

---
+++ script.js
@@ -0,0 +1,22 @@
+{
+
+let user = 'João';
+let password = 'aviao123';
+
+let usuario = prompt ("Digite o seu nome de usuário");
+let senha = prompt ("Digite a sua senha");
+
+
+if (user == usuario && password == senha) {
+ alert ("Login efetuado com sucesso!");
+}
+else if(user != usuario  &&  password == senha){
+    alert("Usuário incorreto"); 
+}
+else if(user == usuario  &&  password != senha){
+    alert("Senha incorreta");
+}
+else {
+    alert("Senha e usuário incorretos"); 
+}
+}
